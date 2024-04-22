3c0d80e37e130b18830a75dfe545677068e81db5
Nandana B Pillai <90125324+Nandanabpillai@users.noreply.github.com>
2024-04-22 21:44:09 +0530

    Update analysis.py

--- views/analysis.py
+++ views/analysis.py
@@ -4,13 +4,23 @@ import plotly.express as px
 import pandas as pd   
 import matplotlib.pyplot as plt
 import seaborn as sns
-st.set_page_config(page_title="Analysis Dashboard", layout="wide")
+st.set_page_config(page_title="Data Visualization")
 def load_view():
+    page_bg_img = """
+    <style>
+        body {
+            width: 100%;
+        }
+    </style>
+    """
+
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
     df = pd.read_csv('turnover.csv')
     # st.dataframe(df)
 
     # MAIN PAGE
-    st.title(':bar_chart: Data Visualisation')
+    st.title(':bar_chart: Data Visualization')
     st.markdown("##")
     avg_satisfy = round(df['satisfaction_level'].mean(), 2)
     avg_lastEval = round(df['last_evaluation'].mean(), 2)
@@ -33,6 +43,7 @@ def load_view():
 
     # Graph 1
     plt.style.use('ggplot')
+    matplotlib.use('agg')
     fig1  = plt.figure(figsize=(3,3), facecolor="black")
     sns.countplot(x='number_project', data=df, hue="left")
     plt.title('Employee Attrition Vs Project Volume', color="white", fontsize=9)
@@ -40,10 +51,13 @@ def load_view():
     plt.ylabel("Count", fontdict=font)
     plt.xticks(fontsize=6, color="white")
     plt.yticks(fontsize=6, color="white")
+    # plt.show()
+   # plt.savefig("num_vol.png")
     #st.pyplot(fig1, use_container_width=False)
 
     # Graph 2
     plt.style.use('ggplot')
+    #matplotlib.use('agg') 
     fig2 = plt.figure(figsize=(3,3), facecolor="black")
     sns.countplot(x='salary', data=df, hue="left")
     plt.title('Employee Turnover by Salary', color="white", fontsize=9)
@@ -51,12 +65,14 @@ def load_view():
     plt.ylabel("Count", fontdict=font)
     plt.xticks(fontsize=6, color="white")
     plt.yticks(fontsize=6, color="white")
+    # plt.show()
     #st.pyplot(fig2, use_container_width=False)
 
     # sns.set(style="whitegrid")
 
     # Create the boxplot - Graph 3
     plt.style.use('ggplot')
+    #matplotlib.use('agg') 
     fig3 = plt.figure(figsize=(3,3), facecolor="black")
     sns.boxplot(x='left', y='satisfaction_level', data=df, hue='left', palette="Set1")
     # plt.rcParams['figure.figsize'] = [4, 4]
@@ -66,9 +82,11 @@ def load_view():
     plt.yticks(fontsize=6, color="white")
     plt.title('Satisfaction Level by Left Status',color="white", fontsize=9)
     # st.pyplot(fig3)
+    # plt.show()
 
     # Graph 4
     plt.style.use('ggplot')
+    #matplotlib.use('agg') 
     fig4 = plt.figure(figsize=(3,3), facecolor="black")
     sns.histplot(x='satisfaction_level', data=df, hue="salary")
     plt.title('Children',color="white", fontsize=9)
@@ -77,11 +95,17 @@ def load_view():
     plt.xticks(fontsize=6, color="white")
     plt.yticks(fontsize=6, color="white")
     plt.legend(['low', 'medium', 'high'],title="Salary", fontsize='7', title_fontsize='7')
+    # plt.show()
 
     lc, rc = st.columns(2)
-    lc.pyplot(fig2, use_container_width=False)
-    rc.pyplot(fig1, use_container_width=False)
+    lc.pyplot(fig2)
+    rc.pyplot(fig1)
+
 
     lc, rc = st.columns(2)
-    lc.pyplot(fig3, use_container_width=False)
-    rc.pyplot(fig4, use_container_width=False)
+    lc.pyplot(fig3)
+    rc.pyplot(fig4)
+# , use_container_width=False
+# , use_container_width=False
+# , use_container_width=False
+# , use_container_width=False
